c795241b0deeb647f56d97907da69a24dffda984
ingeniero26 <ingjerson2014@gmail.com>
2022-09-08 20:54:50 -0500

    rutas productos get

--- routes/api.php
+++ routes/api.php
@@ -22,3 +22,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get("/prueba",function(){
     return \App\Models\Producto::all();
 });
+
+Route::get('productos', [App\Http\Controllers\ProductoController::class, 'index']);
